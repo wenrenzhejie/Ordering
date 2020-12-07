7e486ed489b6642bbcf6fce27ecb3197f1e59fd1
xin <xin@qq.com>
2020-12-07 17:03:58 +0800

    完成小程序端菜品分类、轮播图、美食列表、上拉分页刷新

--- mina/pages/food/index.js
+++ mina/pages/food/index.js
@@ -15,6 +15,8 @@ Page({
         scrollTop: "0",
         loadingMoreHidden: true,
         searchInput: '',
+        p:1,
+        processing:false
     },
     onLoad: function () {
         var that = this;
@@ -23,7 +25,7 @@ Page({
             title: app.globalData.shopName
         });
 
-        that.setData({
+        /*that.setData({
             banners: [
                 {
                     "id": 1,
@@ -39,7 +41,7 @@ Page({
                 }
             ],
             categories: [
-                {id: 0, name: "全部"},
+                {id: 0, name: "全hhhh部"},
                 {id: 1, name: "川菜"},
                 {id: 2, name: "东北菜"},
             ],
@@ -68,7 +70,7 @@ Page({
 			                },
 			                {
 			                    "id": 4,
-			                    "name": "小鸡炖蘑菇-1",
+			                    "name": "小鸡炖蘑hhhhh菇-1",
 			                    "min_price": "15.00",
 			                    "price": "15.00",
 			                    "pic_url": "/images/food.jpg"
@@ -77,6 +79,8 @@ Page({
 			 ],
             loadingMoreHidden: false
         });
+        console.log("aaaaaaaaaaaaaaaaa");*/
+        this.getBannerAndCat();
     },
     scroll: function (e) {
         var that = this, scrollTop = that.data.scrollTop;
@@ -113,6 +117,88 @@ Page({
     toDetailsTap: function (e) {
         wx.navigateTo({
             url: "/pages/food/info?id=" + e.currentTarget.dataset.id
+        });
+    },
+    getBannerAndCat: function () {
+        var that = this;
+        wx.request({
+            url: app.buildUrl("/food/index"),
+            header: app.getRequestHeader(),
+            success: function (res) {
+                var resp = res.data;
+                if (resp.code != 200) {
+                    app.alert({"content": resp.msg});
+                    return;
+                }
+
+                that.setData({
+                    banners: resp.data.banner_list,
+                    categories: resp.data.cat_list
+                });
+                that.getFoodList();
+            }
+        });
+    },
+    catClick: function (e) {
+        this.setData({
+            activeCategoryId: e.currentTarget.id
+        });
+        this.setData({
+            loadingMoreHidden: true,
+            p:1,
+            goods:[]
+        });
+        this.getFoodList();
+    },
+    onReachBottom: function () {
+        var that = this;
+        setTimeout(function () {
+            that.getFoodList();
+        }, 500);
+    },
+    getFoodList: function () {
+        var that = this;
+        if( that.data.processing ){
+            return;
+        }
+
+        if( !that.data.loadingMoreHidden ){
+            return;
+        }
+
+        that.setData({
+            processing:true
+        });
+
+        wx.request({
+            url: app.buildUrl("/food/search"),
+            header: app.getRequestHeader(),
+            data: {
+                cat_id: that.data.activeCategoryId,
+                mix_kw: that.data.searchInput,
+                p: that.data.p,
+            },
+            success: function (res) {
+                var resp = res.data;
+                if (resp.code != 200) {
+                    app.alert({"content": resp.msg});
+                    return;
+                }
+
+                var goods = resp.data.list;
+                that.setData({
+                    goods: that.data.goods.concat( goods ),
+                    p: that.data.p + 1,
+                    processing:false
+                });
+
+                if( resp.data.has_more == 0 ){
+                    that.setData({
+                        loadingMoreHidden: false
+                    });
+                }
+
+            }
         });
     }
 });
--- web/controllers/api/__init__.py
+++ web/controllers/api/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 route_api = Blueprint("api_page",__name__)
 from web.controllers.api.Member import *
-
+from web.controllers.api.Food import *
 @route_api.route("/")
 def index():
     return "mina api V1.0"
